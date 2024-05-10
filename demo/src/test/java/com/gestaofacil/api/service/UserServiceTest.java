daa0d12d1a91a385f64acbd419782d7f00338faf
luizreboucas <primoneto@gmail.com>
2024-05-10 19:07:30 -0300

    teste controller

--- demo/src/test/java/com/gestaofacil/api/service/UserServiceTest.java
+++ demo/src/test/java/com/gestaofacil/api/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.gestaofacil.api.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gestaofacil.api.controllers.UserController;
 import com.gestaofacil.api.domain.company.Company;
 import com.gestaofacil.api.domain.company.CompanyCreationDTO;
 import com.gestaofacil.api.domain.company.CompanyRepository;
@@ -18,47 +20,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import org.assertj.core.api.Assertions;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.net.URI;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
 @ExtendWith(MockitoExtension.class)
-@ActiveProfiles("test")
-@DataJpaTest
-@AutoConfigureTestEntityManager
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserServiceTest {
-    @Mock
-    private PasswordEncoder bcrypt;
+    @MockBean
+    UserService userService;
     @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private CompanyRepository companyRepository;
-    @Autowired
-    EntityManager entityManager;
+    MockMvc mockMvc;
+
     @Test
     @DisplayName("admin user should create a admin account/service")
-    public void UC1cenario01() {
+    public void UC1cenario01() throws Exception {
+
         var companyData = new CompanyCreationDTO("company","1324345","12342342","CE","423","perto","rua adidas");
         Company company = new Company(companyData);
-        entityManager.persist(company);
+        company.setId(1L);
         var userData = new UserCreationDTO( RoleEnum.Admin,"luiz","luiz@email",company.getId(),"123","3123432","8532323");
         User user = new User(userData, company);
-        entityManager.persist(user);
-        var userFromDb = userRepository.findById(user.getUser_id()).get();
-        Assertions.assertThat(user).isEqualTo(userFromDb);
+        when(this.userService.createUser(userData)).thenReturn(new UserDTO(user));
+        this.mockMvc.perform(post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper()
+                        .writeValueAsString(new ObjectMapper().writeValueAsString(userData))))
+                .andExpect(status().isCreated());
+
     }
 
     private User createUser(UserCreationDTO userData, Company company){
         var user = new User(userData, company);
-        entityManager.persist(user);
         return user;
     }
 
     private Company createCompany(CompanyCreationDTO companyData){
         var company = new Company(companyData);
-        entityManager.persist(company);
         return company;
     }
 
